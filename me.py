eb2c31c7f0ae05cd1bbecacfc5365ff6007384ee
henriquevoni <voni.henrique@gmail.com>
2020-12-30 18:58:19 -0300

    helper functions

--- me.py
+++ me.py
@@ -3,10 +3,10 @@ from sklearn.datasets import load_iris
 from sklearn.cluster import KMeans
 
 GATING_THRESHOLD = .3
+N_EXPERTS = 3
 
 X,y = load_iris(return_X_y=True)
 
-n_clusters = 3
 
 def _multivariate_normal_pdf(x, center, cov):
 
@@ -22,32 +22,28 @@ def _multivariate_normal_pdf(x, center, cov):
 def multivariate_normal_pdf(X, center, cov):
     return np.apply_along_axis(_multivariate_normal_pdf, 1, X, center, cov)
 
-def softmax(Z):
+def _softmax(Z):
     exps = np.exp(Z - np.max(Z))
-    return exps / np.sum(exps)
+    return exps / exps.sum()
 
 def compute_g(P):
-    return softmax(P) 
+    return _softmax(P) 
 
 def distribute_samples(X):
-
-    mean_x = np.mean(X, axis=0)
+    
+    centers = _generate_prototypes(X, N_EXPERTS)
     cov_x = np.cov(X.T)
-    p_x = multivariate_normal_pdf(X, mean_x, cov_x)
+    
+    for prototype in centers:
+        p_x = multivariate_normal_pdf(X, prototype, cov_x)
+        g_x = compute_g(p_x)
+        ## selecionar amostras baseado no threshold.
 
-    g_x = compute_g(p_x)
 
-    ## selecionar amostras baseado no threshold.
+def _generate_prototypes(X, n_experts):
+    kmeans = KMeans(n_clusters=n_experts, random_state=10)
+    kmeans.fit(X)
+    return kmeans.cluster_centers_
 
-
-
-# kmeans = KMeans(n_clusters=n_clusters, random_state=10)
-
-# kmeans.fit(X)
-
-# groups = []
-
-# for (i, center) in enumerate(kmeans.cluster_centers_):
 #     group_idx = np.argwhere(kmeans.labels_ == i)
 #     group_samples = np.take(X, group_idx, axis=0)
-#     groups.append( (center, group_samples) )
