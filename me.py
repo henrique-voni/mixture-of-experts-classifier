8bb91764d88317ced9d0483e0d7e030cbe49dd58
henriquevoni <voni.henrique@gmail.com>
2021-01-27 00:30:21 -0300

    Documentando métodos internos

--- me.py
+++ me.py
@@ -20,10 +20,16 @@ class MixtureOfExperts():
         self._gaussian_threshold = gt
         self._params = []
         self._pdfs = []
-        self._X_per_cluster_idx = [] # Para armazenar o indice das amostras por cluster
         self._random_state = random_state
         self._km = KMeans(n_clusters = len(estimators), random_state=random_state)
 
+
+    """
+        Método auxiliar para geração dos parâmetros centro e matrizes de covariância associados a cada especialista.
+
+        Parâmetros: 
+            X - Amostras
+    """
     def _generate_params(self, X):
         self._km.fit(X)
         for i, center in enumerate(self._km.cluster_centers_):
@@ -31,8 +37,19 @@ class MixtureOfExperts():
             cluster_cov = np.cov(cluster_samples.T)
             self._params.append((center, cluster_cov))
 
-    def _distribute_train(self, X, y):
+
+    """
+        Método para distribuição de amostras para cada especialista no treinamento
         
+        Parâmetros:
+            X - Amostras
+            y - classes de cada amostra
+        
+        Primeiro, o método gera os centros e matrizes de covariância associados a cada especialista/cluster. Em seguida,
+        calcula-se a probabilidade usando a função densidade de probabilidade da distribuição normal multivariada para
+        cada amostra. A amostra é aceita pelo cluster se ela for maior que um limiar pré-definido (_gaussian_threshold).
+    """
+    def _distribute_train(self, X, y):      
         dist = []
         self._generate_params(X)
         for (center, cov) in self._params:
@@ -47,6 +64,16 @@ class MixtureOfExperts():
             dist.append({"X" : X_rel, "y" : y_rel})
         return dist
 
+
+    """
+        Método auxiliar para calcular a probabilidade de uma amostra seguir uma distribuição normal multivariada com parâmetros
+        de centro e matriz de covariância.
+
+        Parâmetros:
+            x - vetor de amostra
+            center - vetor de centróide
+            cov - matriz de covariância
+    """
     def _mvpdf_single(self, x, center, cov):
         k = len(x)
         det_cov = np.linalg.det(2 * cov) # Cov multiplicada por 2 para evitar valores muito baixos nas probabilidades
@@ -56,15 +83,39 @@ class MixtureOfExperts():
         p = np.exp( -.5 * ( np.dot( (x - center).T, inv_cov ).dot( x-center ) ))
         return o * p
 
+    """
+        Método para calcular a PDF multivariada para N amostras.
+
+        Parâmetros:
+            X - amostras
+            center - vetor de centróide
+            cov - matriz de covariância
+    """
     def _mvpdf(self, X, center, cov):
         return np.apply_along_axis(self._mvpdf_single, 1, X, center, cov)
 
+    """
+        Método para calcular a função softmax de Z valores.
+
+        Parâmetros:
+            Z - valores de saída a serem aplicados na softmax
+    """
     def _softmax(self, Z):
         exps = np.exp(Z - np.max(Z))
         return exps / exps.sum()
 
+
+    """
+        Método para computar os valores de G associados a cada amostra, dados seus valores de probabilidade.
+
+        @TODO: Considerar não apenas a softmax, mas também normalização por valor/total de valores
+
+        Parâmetros:
+            P - vetor de probabilidades obtidas pela MVPDF
+    """
     def _compute_g(self, P):
         return self._softmax(P) 
+
 
     def fit(self, X, y):   
         for estimator, (X_sub,y_sub) in zip(self._estimators, self._distribute_train(X,y)):
@@ -72,8 +123,8 @@ class MixtureOfExperts():
         print("Estimators fitted successfully.")
 
 
-    def predict(X):
-
+    def predict(self, X):
+        pass
 
 
 
